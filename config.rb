c6b97694b2a93c373d289ad95fa7e152915d8845
Monica Nabais <monicamnabais@gmail.com>
2018-05-14 16:50:31 +0100

    flat dynamic view exercise

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+["Monica-Nabais", "catarinacota", "mariacastro96"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
